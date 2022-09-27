234260b48817c700140d9b91e211ca618f040169
Segev Finer <segev208@gmail.com>
2022-09-27 23:11:11 +0300

    Linux implementation

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
-fn main() {
-    println!("Hello, world!");
+use std::{error::Error, io};
+
+use awake_rs::{AwakeOptions, Awake};
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let _awake = Awake::new(&AwakeOptions{
+        idle: true,
+        ..Default::default()
+    })?;
+
+    let mut buf = String::new();
+    io::stdin().read_line(&mut buf)?;
+
+    Ok(())
 }
--- src/sys/linux.rs
+++ src/sys/linux.rs
@@ -1,6 +1,97 @@
-//! Using systemd [Inhibitor Locks].
-//! For VT, should we use `setterm -blank`, is there something better?
+//! Using [`org.freedesktop.ScreenSaver`], and [systemd Inhibitor Locks].
 //!
-//! [Inhibitor Locks]:(https://www.freedesktop.org/wiki/Software/systemd/inhibit/
+//! For VT, should we add support for using `setterm -blank`? is there something better?
+//!
+//! [ScreenSaver]: https://people.freedesktop.org/~hadess/idle-inhibition-spec/re01.html
+//! [systemd Inhibitor Locks]:(https://www.freedesktop.org/wiki/Software/systemd/inhibit/
 
-todo!();
+use std::{error::Error, time::Duration};
+
+use dbus::blocking::{Connection, Proxy};
+
+use crate::AwakeOptions;
+
+pub struct Awake {
+    options: AwakeOptions,
+    session_conn: Option<Connection>,
+    cookie: Option<u32>,
+
+    system_conn: Option<Connection>,
+    fd: Option<dbus::arg::OwnedFd>,
+}
+
+impl Awake {
+    pub fn new(options: &AwakeOptions) -> Result<Self, Box<dyn Error>> {
+        let mut awake = Awake {
+            options: *options,
+            cookie: None,
+            session_conn: None,
+            system_conn: None,
+            fd: None,
+        };
+        awake.set()?;
+        Ok(awake)
+    }
+
+    fn set(&mut self) -> Result<(), Box<dyn Error>> {
+        self.cookie = if self.options.display {
+            self.session_conn = Some(Connection::new_session()?);
+            let result: (u32,) = self.screensaver_proxy().method_call(
+                "org.freedesktop.ScreenSaver",
+                "Inhibit",
+                ("io.github.segevfiner.awake-rs", "User Requested"),
+            )?;
+            Some(result.0)
+        } else {
+            None
+        };
+
+        self.fd = if self.options.idle {
+            self.system_conn = Some(Connection::new_system()?);
+            let result: (dbus::arg::OwnedFd,) = self.login_proxy().method_call(
+                "org.freedesktop.login1.Manager",
+                "Inhibit",
+                ("idle", "awake-rs", "User Requested", "block"),
+            )?;
+            Some(result.0)
+        } else {
+            None
+        };
+
+        Ok(())
+    }
+
+    fn screensaver_proxy(&self) -> Proxy<&Connection> {
+        self.session_conn.as_ref().unwrap().with_proxy(
+            "org.freedesktop.ScreenSaver",
+            "/org/freedesktop/ScreenSaver",
+            Duration::from_millis(5000),
+        )
+    }
+
+    fn login_proxy(&self) -> Proxy<&Connection> {
+        self.system_conn.as_ref().unwrap().with_proxy(
+            "org.freedesktop.login1",
+            "/org/freedesktop/login1",
+            Duration::from_millis(5000),
+        )
+    }
+}
+
+impl Drop for Awake {
+    fn drop(&mut self) {
+        #[allow(clippy::let_unit_value)]
+        if self.options.display {
+            () = self
+                .screensaver_proxy()
+                .method_call(
+                    "org.freedesktop.ScreenSaver",
+                    "UnInhibit",
+                    (self.cookie.unwrap(),),
+                )
+                .unwrap();
+        }
+
+        if self.options.idle {}
+    }
+}
