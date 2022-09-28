ea1f54d525f23f8315b4d8e560f19ec6013dd239
Segev Finer <segev208@gmail.com>
2022-09-28 10:17:53 +0300

    Convert to zbus

--- src/sys/linux.rs
+++ src/sys/linux.rs
@@ -7,30 +7,61 @@
 //! [ScreenSaver]: https://people.freedesktop.org/~hadess/idle-inhibition-spec/re01.html
 //! [systemd Inhibitor Locks]:(https://www.freedesktop.org/wiki/Software/systemd/inhibit/
 
-use std::{error::Error, time::Duration};
+use std::error::Error;
 
-use dbus::blocking::{Connection, Proxy};
+use zbus::{blocking::Connection, dbus_proxy};
 
 use crate::AwakeOptions;
+
+#[dbus_proxy(
+    interface = "org.freedesktop.login1.Manager",
+    default_service = "org.freedesktop.login1",
+    default_path = "/org/freedesktop/login1"
+)]
+trait Manager {
+    /// Inhibit method
+    fn inhibit(
+        &self,
+        what: &str,
+        who: &str,
+        why: &str,
+        mode: &str,
+    ) -> zbus::Result<zbus::zvariant::OwnedFd>;
+}
+
+#[dbus_proxy]
+trait ScreenSaver {
+    /// Inhibit method
+    fn inhibit(&self, application_name: &str, reason_for_inhibit: &str) -> zbus::Result<u32>;
+
+    /// UnInhibit method
+    fn un_inhibit(&self, cookie: u32) -> zbus::Result<()>;
+}
 
 pub struct Awake {
     options: AwakeOptions,
 
     session_conn: Option<Connection>,
+    screensaver_proxy: Option<ScreenSaverProxyBlocking<'static>>,
     cookie: Option<u32>,
 
     system_conn: Option<Connection>,
-    idle_fd: Option<dbus::arg::OwnedFd>,
-    sleep_fd: Option<dbus::arg::OwnedFd>,
+    manager_proxy: Option<ManagerProxyBlocking<'static>>,
+    idle_fd: Option<zbus::zvariant::OwnedFd>,
+    sleep_fd: Option<zbus::zvariant::OwnedFd>,
 }
 
 impl Awake {
     pub fn new(options: &AwakeOptions) -> Result<Self, Box<dyn Error>> {
         let mut awake = Awake {
             options: *options,
-            cookie: None,
+
             session_conn: None,
+            screensaver_proxy: None,
+            cookie: None,
+
             system_conn: None,
+            manager_proxy: None,
             idle_fd: None,
             sleep_fd: None,
         };
@@ -38,75 +69,61 @@ impl Awake {
         Ok(awake)
     }
 
-    fn set(&mut self) -> Result<(), Box<dyn Error>> {
+    fn set(&mut self) -> Result<(), zbus::Error> {
         self.cookie = if self.options.display {
-            self.session_conn = Some(Connection::new_session()?);
-            let result: (u32,) = self.screensaver_proxy().method_call(
-                "org.freedesktop.ScreenSaver",
-                "Inhibit",
-                ("io.github.segevfiner.keepawake-rs", "User requested"),
-            )?;
-            Some(result.0)
+            self.session_conn = Some(Connection::session()?);
+            self.screensaver_proxy = Some(ScreenSaverProxyBlocking::new(
+                self.session_conn.as_ref().unwrap(),
+            )?);
+            Some(
+                self.screensaver_proxy
+                    .as_ref()
+                    .unwrap()
+                    .inhibit("io.github.segevfiner.keepawake-rs", "User requested")?,
+            )
         } else {
             None
         };
 
+        if self.options.idle || self.options.sleep {
+            self.system_conn = Some(Connection::system()?);
+            self.manager_proxy = Some(ManagerProxyBlocking::new(
+                self.system_conn.as_ref().unwrap(),
+            )?);
+        }
+
         self.idle_fd = if self.options.idle {
-            self.system_conn = Some(Connection::new_system()?);
-            let result: (dbus::arg::OwnedFd,) = self.login_proxy().method_call(
-                "org.freedesktop.login1.Manager",
-                "Inhibit",
-                ("idle", "keepawake-rs", "User requested", "block"),
-            )?;
-            Some(result.0)
+            Some(self.manager_proxy.as_ref().unwrap().inhibit(
+                "idle",
+                "keepawake-rs",
+                "User requested",
+                "block",
+            )?)
         } else {
             None
         };
 
         self.sleep_fd = if self.options.sleep {
-            self.system_conn = Some(Connection::new_system()?);
-            let result: (dbus::arg::OwnedFd,) = self.login_proxy().method_call(
-                "org.freedesktop.login1.Manager",
-                "Inhibit",
-                ("sleep", "keepawake-rs", "User requested", "block"),
-            )?;
-            Some(result.0)
+            Some(self.manager_proxy.as_ref().unwrap().inhibit(
+                "sleep",
+                "keepawake-rs",
+                "User requested",
+                "block",
+            )?)
         } else {
             None
         };
 
         Ok(())
     }
-
-    fn screensaver_proxy(&self) -> Proxy<&Connection> {
-        self.session_conn.as_ref().unwrap().with_proxy(
-            "org.freedesktop.ScreenSaver",
-            "/org/freedesktop/ScreenSaver",
-            Duration::from_millis(5000),
-        )
-    }
-
-    fn login_proxy(&self) -> Proxy<&Connection> {
-        self.system_conn.as_ref().unwrap().with_proxy(
-            "org.freedesktop.login1",
-            "/org/freedesktop/login1",
-            Duration::from_millis(5000),
-        )
-    }
 }
 
 impl Drop for Awake {
     fn drop(&mut self) {
-        #[allow(clippy::let_unit_value)]
         if self.options.display {
-            () = self
-                .screensaver_proxy()
-                .method_call(
-                    "org.freedesktop.ScreenSaver",
-                    "UnInhibit",
-                    (self.cookie.unwrap(),),
-                )
-                .unwrap();
+            if let Some(p) = self.screensaver_proxy.as_ref() {
+                p.un_inhibit(self.cookie.unwrap()).unwrap()
+            }
         }
     }
 }
