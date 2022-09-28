14043cf48cef07d5bfd7cbb3e113855eb90e3ac7
Segev Finer <segev208@gmail.com>
2022-09-29 01:43:03 +0300

    Docs

--- src/sys/macos.rs
+++ src/sys/macos.rs
@@ -1,5 +1,7 @@
 //! Using [`IOPMAssertionCreateWithName`].
 //!
+//! Debug with `pmset -g assertions`.
+//!
 //! [`IOPMAssertionCreateWithName`]: https://developer.apple.com/documentation/iokit/1557134-iopmassertioncreatewithname
 
 todo!();
