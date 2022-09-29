d39ff4636dda5e28d44b8d20d440474030495e9c
Segev Finer <segev208@gmail.com>
2022-09-30 00:55:09 +0300

    Implement CLI

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,40 @@
-use std::{error::Error, io};
+use std::{error::Error, sync::mpsc::channel};
 
-use keepawake::{AwakeOptions, Awake};
+use keepawake::{Awake, AwakeOptions};
+
+use clap::Parser;
+
+#[derive(Parser)]
+#[command(author, version, about, long_about = None)]
+struct Cli {
+    /// Keep display on
+    #[arg(short, long)]
+    display: bool,
+
+    /// Keep system from idle sleeping
+    #[arg(short, long)]
+    idle: bool,
+
+    /// Keep system from sleeping (Functionality and conditions for this to work vary by OS)
+    #[arg(short, long)]
+    sleep: bool,
+}
 
 fn main() -> Result<(), Box<dyn Error>> {
-    // TODO CLI
-    let _awake = Awake::new(&AwakeOptions{
-        display: true,
-        idle: true,
-        ..Default::default()
+    let cli = Cli::parse();
+
+    let (tx, rx) = channel();
+
+    ctrlc::set_handler(move || tx.send(()).expect("Could not send signal on channel."))
+        .expect("Error setting Ctrl-C handler");
+
+    let _awake = Awake::new(&AwakeOptions {
+        display: cli.display,
+        idle: cli.idle,
+        sleep: cli.sleep,
     })?;
 
-    let mut buf = String::new();
-    io::stdin().read_line(&mut buf)?;
+    rx.recv().expect("Could not receive from channel.");
 
     Ok(())
 }
