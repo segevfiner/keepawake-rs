704c8f22b9e2f7befb583497cc51cf25f6d75bfc
Segev Finer <segev208@gmail.com>
2022-10-15 17:23:25 +0300

    Upgrade dependencies and shadow-rs

--- build.rs
+++ build.rs
@@ -1,7 +1,7 @@
 use std::error::Error;
 
 fn main() -> Result<(), Box<dyn Error>> {
-    shadow_rs::new().map_err(|err| err.to_string())?;
+    shadow_rs::new()?;
 
     if cfg!(target_os = "windows") {
         winres::WindowsResource::new().set_manifest_file("keepawake.exe.manifest").compile()?;
