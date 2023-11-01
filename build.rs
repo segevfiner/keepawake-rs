91409c6db59cbb488076201e53493c5953e1b794
Segev Finer <segev208@gmail.com>
2023-11-01 09:29:10 +0200

    Only include manifest with bin feature

--- build.rs
+++ build.rs
@@ -3,7 +3,7 @@ use std::error::Error;
 fn main() -> Result<(), Box<dyn Error>> {
     shadow_rs::new()?;
 
-    if cfg!(target_os = "windows") {
+    if cfg!(all(feature = "bin", target_os = "windows")) {
         winres::WindowsResource::new()
             .set_manifest_file("keepawake.exe.manifest")
             .compile()?;
