a1e833dc2baf150fe9b922f1c7c24ffc6ab3f332
Segev Finer <segev208@gmail.com>
2023-11-01 09:43:59 +0200

    Migrate to winresource

--- build.rs
+++ build.rs
@@ -1,10 +1,11 @@
 use std::error::Error;
+use std::env;
 
 fn main() -> Result<(), Box<dyn Error>> {
     shadow_rs::new()?;
 
-    if cfg!(all(feature = "bin", target_os = "windows")) {
-        winres::WindowsResource::new()
+    if env::var("CARGO_FEATURE_BIN").is_ok() && env::var("CARGO_CFG_TARGET_OS").unwrap() == "windows" {
+        winresource::WindowsResource::new()
             .set_manifest_file("keepawake.exe.manifest")
             .compile()?;
     }
