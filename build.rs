fd873d890bd55d3d13951a45f6e9971c95365089
Lane Kolbly <lane@rscheme.org>
2023-11-05 11:14:03 -0600

    Make shadow-rs build dependency optional

--- build.rs
+++ build.rs
@@ -2,6 +2,7 @@ use std::error::Error;
 use std::env;
 
 fn main() -> Result<(), Box<dyn Error>> {
+    #[cfg(feature = "bin")]
     shadow_rs::new()?;
 
     if env::var("CARGO_FEATURE_BIN").is_ok() && env::var("CARGO_CFG_TARGET_OS").unwrap() == "windows" {
