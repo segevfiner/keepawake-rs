96184c327516ad38779e7b379f7d439c1c6f2b0a
Segev Finer <segev208@gmail.com>
2023-11-05 22:32:34 +0200

    Update build.rs

--- build.rs
+++ build.rs
@@ -5,7 +5,8 @@ fn main() -> Result<(), Box<dyn Error>> {
     #[cfg(feature = "bin")]
     shadow_rs::new()?;
 
-    if env::var("CARGO_FEATURE_BIN").is_ok() && env::var("CARGO_CFG_TARGET_OS").unwrap() == "windows" {
+    #[cfg(feature = "bin")]
+    if env::var("CARGO_CFG_TARGET_OS").unwrap() == "windows" {
         winresource::WindowsResource::new()
             .set_manifest_file("keepawake.exe.manifest")
             .compile()?;
