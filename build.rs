a60330bc95f845dd2bc8919fc785c2338072af8b
Segev Finer <segev208@gmail.com>
2025-01-18 02:22:49 +0200

    Cleanup

--- build.rs
+++ build.rs
@@ -7,7 +7,7 @@ use shadow_rs::ShadowBuilder;
 
 fn main() -> Result<(), Box<dyn Error>> {
     #[cfg(feature = "bin")]
-    ShadowBuilder::builder().build().unwrap();
+    ShadowBuilder::builder().build()?;
 
     #[cfg(feature = "bin")]
     if env::var("CARGO_CFG_TARGET_OS").unwrap() == "windows" {
